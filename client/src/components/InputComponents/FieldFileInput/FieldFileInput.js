cfb746c1dc050b330392b16ea6feb35a0f37e651
EvgenStr <juve2910@gmail.com>
2021-08-21 02:38:13 +0300

    fix FieldFileInput submit file problem

--- client/src/components/InputComponents/FieldFileInput/FieldFileInput.js
+++ client/src/components/InputComponents/FieldFileInput/FieldFileInput.js
@@ -1,38 +1,41 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import { Field } from 'formik';
 
 const FieldFileInput = ({ classes, ...rest }) => {
-  const {
-    fileUploadContainer, labelClass, fileNameClass, fileInput,
-  } = classes;
+  const fileInput1 = useRef(null);
+  const [selectedFile, setSelectedFile] = useState(undefined);
+  const { fileUploadContainer, labelClass, fileNameClass, fileInput } = classes;
 
   return (
     <Field name={rest.name}>
-      {(props) => {
-        const {
-          field,
-        } = props;
-
+      {props => {
+        const { field } = props;
         const getFileName = () => {
           if (props.field.value) {
             return props.field.value.name;
           }
           return '';
         };
+        const onChange = e => {
+          props.form.setFieldValue('file', e.target.files[0]);
+        };
 
         return (
           <div className={fileUploadContainer}>
-            <label htmlFor="fileInput" className={labelClass}>
+            <label htmlFor='fileInput' className={labelClass}>
               Choose file
             </label>
-            <span id="fileNameContainer" className={fileNameClass}>
+            <span id='fileNameContainer' className={fileNameClass}>
               {getFileName()}
             </span>
             <input
               {...field}
+              ref={fileInput1}
               className={fileInput}
-              id="fileInput"
-              type="file"
+              id='fileInput'
+              type='file'
+              value={selectedFile}
+              onChange={onChange}
             />
           </div>
         );
