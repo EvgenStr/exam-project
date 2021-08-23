793d412caa60980ca376b0795feefed421e80642
EvgenStr <juve2910@gmail.com>
2021-08-23 18:06:32 +0300

    refactor and delete console.logs

--- server/src/controllers/queries/userQueries.js
+++ server/src/controllers/queries/userQueries.js
@@ -4,10 +4,13 @@ const ServerError = require('../../errors/ServerError');
 const bcrypt = require('bcrypt');
 
 module.exports.updateUser = async (data, userId, transaction) => {
-  const [updatedCount, [updatedUser]] = await db.User.update(data,
-    { where: { id: userId }, returning: true, transaction });
+  const [updatedCount, [updatedUser]] = await db.User.update(data, {
+    where: { id: userId },
+    returning: true,
+    transaction,
+  });
   if (updatedCount !== 1) {
-    throw new ServerError('cannot update user');
+    throw new ServerError('Cannot update user');
   }
   return updatedUser.dataValues;
 };
@@ -15,16 +18,16 @@ module.exports.updateUser = async (data, userId, transaction) => {
 module.exports.findUser = async (predicate, transaction) => {
   const result = await db.User.findOne({ where: predicate, transaction });
   if (!result) {
-    throw new NotFound('user with this data didn`t exist');
+    throw new NotFound("User with this data didn't exist");
   } else {
     return result.get({ plain: true });
   }
 };
 
-module.exports.userCreation = async (data) => {
+module.exports.userCreation = async data => {
   const newUser = await db.User.create(data);
   if (!newUser) {
-    throw new ServerError('server error on user creation');
+    throw new ServerError('Server error on user creation');
   } else {
     return newUser.get({ plain: true });
   }
@@ -33,6 +36,6 @@ module.exports.userCreation = async (data) => {
 module.exports.passwordCompare = async (pass1, pass2) => {
   const passwordCompare = await bcrypt.compare(pass1, pass2);
   if (!passwordCompare) {
-    throw new NotFound('Wrong password');
+    throw new NotFound('Password mismatch');
   }
 };
