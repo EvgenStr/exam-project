cb4aaf4c396c073116738ee2c0aa70fa6026c6e6
EvgenStr <juve2910@gmail.com>
2021-08-24 19:25:34 +0300

    add paginate middleware and userController in contest router

--- server/src/router/contests.js
+++ server/src/router/contests.js
@@ -1,20 +1,22 @@
 const contestRouter = require('express').Router();
 const contestController = require('../controllers/contestController');
+const userController = require('../controllers/userController');
 const basicMiddlewares = require('../middlewares/basicMiddlewares');
+const paginate = require('../middlewares/paginate');
 const upload = require('../utils/fileUpload');
 
 contestRouter.get('/data', contestController.dataForContest); ///dataForContest
 contestRouter.post(
-  '/pay', /* '/' */
+  '/pay' /* '/' */,
   basicMiddlewares.onlyForCustomer,
   upload.uploadContestFiles,
   basicMiddlewares.parseBody,
   validators.validateContestCreation,
   userController.payment,
 );
-
 contestRouter.get(
   '/customer/:userId/:status',
+  paginate,
   contestController.getCustomersContests,
 );
 contestRouter.get(
@@ -26,6 +28,17 @@ contestRouter.get(
   '/',
   basicMiddlewares.onlyForCreative,
   contestController.getContests,
+);
+contestRouter.patch(
+  '/:contestId',
+  upload.updateContestFile,
+  contestController.updateContest,
+);
+contestRouter.post(
+  '/offer',
+  upload.uploadLogoFiles,
+  basicMiddlewares.canSendOffer,
+  contestController.setNewOffer,
 );
 
 module.exports = contestRouter;
