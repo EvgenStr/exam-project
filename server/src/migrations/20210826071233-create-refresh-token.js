ae2500508b4aa7e721dee17e026aff2b56dfc3f3
EvgenStr <juve2910@gmail.com>
2021-08-28 16:06:51 +0300

    format migrations and delete use strict

--- server/src/migrations/20210826071233-create-refresh-token.js
+++ server/src/migrations/20210826071233-create-refresh-token.js
@@ -1,4 +1,3 @@
-'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('refresh_tokens', {
@@ -6,7 +5,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       userId: {
         type: Sequelize.INTEGER,
@@ -23,22 +22,22 @@ module.exports = {
         allowNull: false,
       },
       ua: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       fingerprint: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
-    })
+        type: Sequelize.DATE,
+      },
+    });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('refresh_tokens')
-  }
-}
+    await queryInterface.dropTable('refresh_tokens');
+  },
+};
