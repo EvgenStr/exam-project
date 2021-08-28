5af588fad9262a168361e69abb533d7a39864cb6
EvgenStr <juve2910@gmail.com>
2021-08-28 15:57:49 +0300

    create message model and migration

---
+++ server/src/migrations/20210828110640-create-messages.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+          onUpdate: 'cascade',
+          onDelete: 'cascade',
+        },
+      },
+      body: {
+        type: Sequelize.STRING(256),
+        allowNull: false,
+      },
+      conversationId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Conversations',
+          key: 'id',
+          onUpdate: 'cascade',
+          onDelete: 'cascade',
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Messages');
+  },
+};
---
+++ server/src/models/message.js
@@ -0,0 +1,44 @@
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Message extends Model {
+    static associate (models) {
+      Message.belongsTo(models.User, { foreignKey: 'userId', sourceKey: 'id' });
+      Message.belongsTo(models.Conversation, {
+        foreignKey: 'conversationId',
+      });
+    }
+  }
+  Message.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+      body: {
+        type: DataTypes.STRING(256),
+        allowNull: false,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      conversationId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Conversations',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Message',
+    },
+  );
+  return Message;
+};
