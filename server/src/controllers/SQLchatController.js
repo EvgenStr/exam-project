e7bcad0a563b732fcb8d6f30f4822a296e8cb671
EvgenStr <juve2910@gmail.com>
2021-08-31 19:22:47 +0300

    add getPreview to new chatController

--- server/src/controllers/SQLchatController.js
+++ server/src/controllers/SQLchatController.js
@@ -1,6 +1,11 @@
 const createHttpError = require('http-errors');
-const { Conversation, Message } = require('../models');
-const User = require('../models/user');
+const {
+  Conversation,
+  Message,
+  User,
+  BlackList,
+  FavoriteList,
+} = require('../models');
 const ConversationsToCatalogs = require('../models/conversationstocatalogs');
 const socketController = require('../socketInit');
 const userQueries = require('./queries/userQueries');
@@ -71,9 +76,7 @@ module.exports.getChat = async (req, res, next) => {
     const conversation = await Conversation.findByPk(conversationId);
     const conversationWithMessages = await conversation.getMessages(); /*pagination*/
     const interlocutor = await User.findByPk(interlocutorId, {
-      attributes: {
-        exclude: ['password', 'email', 'role', 'balance', 'rating'],
-      },
+      attributes: ['id', 'firstName', 'lastName', 'displayName', 'avatar'],
     });
     res.send({ conversationWithMessages, interlocutor });
   } catch (e) {
@@ -83,6 +86,42 @@ module.exports.getChat = async (req, res, next) => {
 
 module.exports.getPreview = async (req, res, next) => {
   try {
+    const {
+      tokenData: { userId, role },
+    } = req;
+
+    const conversations = await Conversation.findAll({
+      where:
+        role === CONSTANTS.CREATOR
+          ? { creatorId: userId }
+          : { customerId: userId },
+      include: [
+        { model: Message, limit: 1 },
+        {
+          model: User,
+          as: role === 'creator' ? 'customer' : 'creator',
+          attributes: ['id', 'firstName', 'lastName', 'displayName', 'avatar'],
+          include: [
+            { model: BlackList, as: 'blockedUser' },
+            { model: FavoriteList, as: 'favoriteUser' },
+          ],
+        },
+      ],
+    });
+
+    const result = conversations.map(conversation => {
+      const prepared = {};
+      prepared.interlocutor =
+        conversation[role === 'creator' ? 'customer' : 'creator'];
+      prepared.blackList = prepared.interlocutor.blockedUser;
+      prepared.sender = conversation.Messages[0].userId;
+      prepared.text = conversation.Messages[0].body;
+      prepared.participants = [conversation.customerId, conversation.creatorId];
+      prepared.favoriteList = prepared.interlocutor.favoriteUser;
+      return prepared;
+    });
+
+    res.send(result);
   } catch (e) {
     next(e);
   }
