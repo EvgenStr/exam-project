c88e5f07bd29cfaba8c603af3b979022aa7e2409
EvgenStr <juve2910@gmail.com>
2021-09-01 17:59:27 +0300

    fix unwork unique constraints in conversations and catalogs migrations

--- server/src/migrations/20210828083952-create-catalogs.js
+++ server/src/migrations/20210828083952-create-catalogs.js
@@ -1,38 +1,28 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable(
-      'Catalogs',
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        name: {
-          type: Sequelize.STRING,
-          allowNull: false,
-        },
-        userId: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: {
-            model: 'Users',
-            key: 'id',
-            onUpdate: 'cascade',
-            onDelete: 'cascade',
-          },
-        },
+    await queryInterface.createTable('Catalogs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
       },
-      {
-        indexes: [
-          {
-            unique: true,
-            fields: ['name', 'userId'],
-          },
-        ],
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: 'catalogs',
       },
-    );
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+          onDelete: 'cascade',
+        },
+        unique: 'catalogs',
+      },
+    });
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Catalogs');
--- server/src/migrations/20210828100454-create-conversations.js
+++ server/src/migrations/20210828100454-create-conversations.js
@@ -1,62 +1,53 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable(
-      'Conversations',
-      {
-        id: {
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-          type: Sequelize.INTEGER,
-        },
-        customerId: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: {
-            model: 'Users',
-            key: 'id',
-            onUpdate: 'cascade',
-            onDelete: 'cascade',
-          },
-        },
-        creatorId: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: {
-            model: 'Users',
-            key: 'id',
-            onUpdate: 'cascade',
-            onDelete: 'cascade',
-          },
-        },
-        blackList:{
-          allowNull: false,
-          type: Sequelize.ARRAY(Sequelize.BOOLEAN),
-          defaultValue: [false, false],
-        },
-        favoriteList:{
-          allowNull: false,
-          type: Sequelize.ARRAY(Sequelize.BOOLEAN),
-          defaultValue: [false, false],
-        },
-        createdAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
-        updatedAt: {
-          allowNull: false,
-          type: Sequelize.DATE,
-        },
+    await queryInterface.createTable('Conversations', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
       },
-      {
-        indexes: [
-          {
-            unique: true,
-            fields: ['customerId', 'creatorId'],
-          },
-        ],
+      customerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+          onUpdate: 'cascade',
+          onDelete: 'cascade',
+        },
+        unique: 'participants',
       },
-    );
+      creatorId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+          key: 'id',
+          onUpdate: 'cascade',
+          onDelete: 'cascade',
+        },
+        unique: 'participants',
+      },
+      blackList: {
+        allowNull: false,
+        type: Sequelize.ARRAY(Sequelize.BOOLEAN),
+        defaultValue: [false, false],
+      },
+      favoriteList: {
+        allowNull: false,
+        type: Sequelize.ARRAY(Sequelize.BOOLEAN),
+        defaultValue: [false, false],
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Conversations');
