b3991863099ec23aebc72f3ca0c835277a880e80
EvgenStr <juve2910@gmail.com>
2021-09-07 13:50:00 +0300

    import useState, useEffect in ModeratorDashboard component

--- client/src/components/ModeratorDashboard/index.js
+++ client/src/components/ModeratorDashboard/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { useSelector, useDispatch } from 'react-redux'
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { moderationActionGetOffers } from '../../actions/actionCreator';
 
 function ModeratorDashboard () {
-
-  const test = useSelector(state => state);
-  const dispatch = useDispatch()
-  console.log(test, 'TEST')
+  const test = useSelector(state => state.moderationOffers);
+  const dispatch = useDispatch();
+  console.log(test, 'TEST');
+  useEffect(() => {
+    console.log('ACTION');
+    dispatch(moderationActionGetOffers());
+  }, []);
   return <div></div>;
 }
 export default ModeratorDashboard;
