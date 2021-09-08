821e67188e5576d33c61ce64c7774ff243481e0f
EvgenStr <juve2910@gmail.com>
2021-09-08 17:41:36 +0300

    add new actions for moderationOffersReducer

--- client/src/reducers/moderationOffersReducer.js
+++ client/src/reducers/moderationOffersReducer.js
@@ -1,12 +1,11 @@
 import ACTION from '../actions/actionTypes';
-import CONSTANTS from '../constants';
 
 const initialState = {
   isFetching: true,
   error: null,
   offers: [],
   count: 0,
- };
+};
 
 function moderationOffersReducer (state = initialState, action) {
   switch (action.type) {
@@ -28,6 +27,15 @@ function moderationOffersReducer (state = initialState, action) {
     }
     case ACTION.GET_OFFERS_FOR_MODERATOR_ERROR: {
       return { ...state, isFetching: false, error: action.error, offers: [] };
+    }
+    case ACTION.SET_OFFER_STATUS_FOR_MODERATOR_REQUEST: {
+      return { ...state, isFetching: true, error: null };
+    }
+    case ACTION.SET_OFFER_STATUS_FOR_MODERATOR_SUCCESS: {
+      return { ...state, isFetching: false, error: null };
+    }
+    case ACTION.SET_OFFER_STATUS_FOR_MODERATOR_ERROR: {
+      return { ...state, isFetching: false, error: action.error };
     }
     default:
       return state;
