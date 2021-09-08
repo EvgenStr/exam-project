77587205a1ad34c0f7bf1ab012afa48e89da0acd
EvgenStr <juve2910@gmail.com>
2021-09-08 17:45:09 +0300

    change limit in paginate middleware

--- server/src/middlewares/paginate.js
+++ server/src/middlewares/paginate.js
@@ -1,10 +1,10 @@
 module.exports = async (req, res, next) => {
   try {
     const {
-      query: { limit = 8, offset = 0 },
+      query: { limit = 10, offset = 0 },
     } = req;
     req.pagination = {
-      limit: parseInt(limit > 8 || limit <= 0 ? 8 : limit),
+      limit: parseInt(limit > 10 || limit <= 0 ? 10 : limit),
       offset: offset < 0 ? 0 : offset,
     };
     next();
