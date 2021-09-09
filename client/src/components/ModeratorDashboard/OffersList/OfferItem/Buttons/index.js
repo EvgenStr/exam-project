84997c126280c90edcb7dc8c9fedd5fbce9a7914
EvgenStr <juve2910@gmail.com>
2021-09-09 15:18:40 +0300

    add constants and fix Buttons styles

--- client/src/components/ModeratorDashboard/OffersList/OfferItem/Buttons/index.js
+++ client/src/components/ModeratorDashboard/OffersList/OfferItem/Buttons/index.js
@@ -3,30 +3,35 @@ import { useDispatch } from 'react-redux';
 import cx from 'classnames';
 import { moderationActionSetStatus } from '../../../../../actions/actionCreator';
 import styles from './Buttons.module.sass';
+import CONSTANTS from '../../../../../constants';
 
-function Buttons ({ id }) {
+function Buttons ({ id, status }) {
   const dispatch = useDispatch();
   const buttonHandler = status => {
     dispatch(moderationActionSetStatus({ status, id }));
   };
   return (
     <div className={styles.buttonsContainer}>
-      <button
-        className={cx(styles.buttons, styles.accept)}
-        onClick={() => {
-          buttonHandler('accepted');
-        }}
-      >
-        Accept
-      </button>
-      <button
-        className={cx(styles.buttons, styles.decline)}
-        onClick={() => {
-          buttonHandler('declined');
-        }}
-      >
-        Decline
-      </button>
+      {status !== CONSTANTS.OFFER_STATUS_ACCEPTED && (
+        <button
+          className={cx(styles.buttons, styles.accept)}
+          onClick={() => {
+            buttonHandler(CONSTANTS.OFFER_STATUS_ACCEPTED);
+          }}
+        >
+          Accept
+        </button>
+      )}
+      {status !== CONSTANTS.OFFER_STATUS_DECLINED && (
+        <button
+          className={cx(styles.buttons, styles.decline)}
+          onClick={() => {
+            buttonHandler(CONSTANTS.OFFER_STATUS_DECLINED);
+          }}
+        >
+          Decline
+        </button>
+      )}
     </div>
   );
 }
