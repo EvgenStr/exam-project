655116072e3e5f863e64b0b3c349785e054525c0
EvgenStr <juve2910@gmail.com>
2021-09-10 18:34:21 +0300

    create ForgotPasswordPage component and styles

---
+++ client/src/pages/ForgotPasswordPage/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ForgotPasswordForm from '../../components/ForgotPasswordForm';
+import Logo from '../../components/Logo';
+import styles from './ForgotPasswordPage.module.sass';
+
+import CONSTANTS from '../../constants';
+
+const ForgotPasswordPage = props => (
+  <div className={styles.mainContainer}>
+    <div className={styles.passwordContainer}>
+      <div className={styles.header}>
+        <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt='logo' />
+        <div className={styles.linkContainer}>
+          <Link to='/registration' style={{ textDecoration: 'none' }}>
+            <span>Signup</span>
+          </Link>
+          <Link to='/login' style={{ textDecoration: 'none' }}>
+            <span>Login</span>
+          </Link>
+        </div>
+      </div>
+      <div className={styles.passwordFormContainer}>
+        <ForgotPasswordForm />
+      </div>
+    </div>
+  </div>
+);
+export default ForgotPasswordPage;
