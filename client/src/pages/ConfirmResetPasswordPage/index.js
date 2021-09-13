1a58bc6a6a19eba33995ed34232f9df558a6fa2c
EvgenStr <juve2910@gmail.com>
2021-09-13 17:54:19 +0300

    create ConfirmResetPasswordPage component and styles

---
+++ client/src/pages/ConfirmResetPasswordPage/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import ConfirmResetPassword from '../../components/ConfirmResetPassword';
+import Logo from '../../components/Logo';
+import CONSTANTS from '../../constants';
+import styles from './ConfirmResetPasswordPage.module.sass';
+
+function ConfirmResetPasswordPage () {
+  return (
+    <div className={styles.container}>
+      <div className={styles.passwordContainer}>
+        <div className={styles.header}>
+          <Logo src={`${CONSTANTS.STATIC_IMAGES_PATH}logo.png`} alt='logo' />
+          <div className={styles.linkContainer}>
+            <Link to='/registration' style={{ textDecoration: 'none' }}>
+              <span>Signup</span>
+            </Link>
+            <Link to='/login' style={{ textDecoration: 'none' }}>
+              <span>Login</span>
+            </Link>
+          </div>
+        </div>
+        <div className={styles.passwordFormContainer}>
+          <ConfirmResetPassword />
+        </div>
+      </div>
+    </div>
+  );
+}
+export default ConfirmResetPasswordPage;
