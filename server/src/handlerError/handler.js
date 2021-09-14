44cca622302fece30305442b4e4d968657fefb99
EvgenStr <juve2910@gmail.com>
2021-09-14 15:07:29 +0300

    refactor error handler

--- server/src/handlerError/handler.js
+++ server/src/handlerError/handler.js
@@ -1,17 +1,14 @@
-module.exports = (err, req, res, next) => {
+module.exports = async (err, req, res, next) => {
   console.log(err);
+  const status = err.status || 500;
   if (
     err.message ===
       'new row for relation "Banks" violates check constraint "Banks_balance_ck"' ||
     err.message ===
       'new row for relation "Users" violates check constraint "Users_balance_ck"'
   ) {
-    err.message = 'Not Enough money';
-    err.code = 406;
+    return res.status(406).send('Not Enough money');
   }
-  if (!err.message || !err.code) {
-    res.status(500).send('Server Error');
-  } else {
-    res.status(err.code).send(err.message);
-  }
+
+  res.status(status).send(err.message || 'Server Error');
 };
