302b7a4066ed9167edb5c394806cb5a0f8ee6b01
EvgenStr <juve2910@gmail.com>
2021-09-14 12:09:16 +0300

    import Error component in ConfirmResetPassword

--- client/src/components/ConfirmResetPassword/index.js
+++ client/src/components/ConfirmResetPassword/index.js
@@ -3,16 +3,23 @@ import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { resetPasswordConfirmAction } from '../../actions/actionCreator';
 import Spinner from '../Spinner/Spinner';
+import Error from '../Error/Error';
+import styles from './ConfirmResetPassword.module.sass';
 
 function ConfirmResetPassword () {
   const { token } = useParams();
   const { data, isFetching, error } = useSelector(state => state.resetPassword);
   const dispatch = useDispatch();
-  console.log(token, 'token');
   useEffect(() => {
-    dispatch(resetPasswordConfirmAction({token}));
+    dispatch(resetPasswordConfirmAction({ token }));
   }, []);
 
-  return <div>TEST</div>;
+  return (
+    <div className={styles.container}>
+      {isFetching && <Spinner />}
+      {error && <Error data={error.data} status={error.status} />}
+      {data && <span>{data}</span>}
+    </div>
+  );
 }
 export default ConfirmResetPassword;
