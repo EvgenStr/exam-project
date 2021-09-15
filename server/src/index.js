d1697a43cdebf7b628ad75b874c358fa96c83b69
EvgenStr <juve2910@gmail.com>
2021-09-15 17:33:47 +0300

    add app.use(logErrors) to index file

--- server/src/index.js
+++ server/src/index.js
@@ -6,6 +6,7 @@ require('./dbMongo/mongoose');
 const router = require('./router');
 const controller = require('./socketInit');
 const handlerError = require('./handlerError/handler');
+const logErrors = require('./utils/logErrors');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/public', express.static('public'));
 app.use(router);
+app.use(logErrors);
 app.use(handlerError);
 
 const server = http.createServer(app);
