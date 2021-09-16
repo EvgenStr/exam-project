6c147d058a3a2bf2c2a17c7b0816e60bb0a9d4b2
EvgenStr <juve2910@gmail.com>
2021-09-16 16:28:19 +0300

    refactor logError , add functions for fs/promises

--- server/src/utils/logErrors.js
+++ server/src/utils/logErrors.js
@@ -1,38 +1,26 @@
-const fs = require('fs');
 const path = require('path');
-const { access, constants, writeFile, readFile } = fs;
 const { LOGS_PATH } = require('../constants');
+const { exists, write, mkdir, read } = require('./functions');
 
 module.exports = async (err, req, res, next) => {
   try {
     const logsPath = path.resolve(LOGS_PATH, 'logs.json');
-    const { message, stack, status: code } = err;
-    const error = { message, stack, code, time: Date.now() };
+    const { message, stack: stackTrace, status: code } = err;
+    const error = { message, stackTrace, code, time: Date.now() };
 
-    await access(logsPath, constants.W_OK, e => {
-      if (e) {
-        return writeFile(
-          logsPath,
-          JSON.stringify([error], null, 2),
-          'utf8',
-          async e => {
-            if (e) throw e;
-          },
-        );
-      }
-      readFile(logsPath, async (e, data) => {
-        if (!e) {
-          const oldErrors = await JSON.parse(data);
-          oldErrors.push(error);
-          const json = JSON.stringify(oldErrors, null, 2);
-          writeFile(logsPath, json, e => {
-            if (e) throw e;
-          });
-        }
-      });
-    });
+    const fileIsExist = await exists(logsPath);
+
+    if (!fileIsExist) {
+      await mkdir(LOGS_PATH);
+      await write(logsPath, [error]);
+      return;
+    }
+    const errors = await read(logsPath);
+    if(!errors) return await write(logsPath, [error]);
+    errors.push(error);
+    await write(logsPath, errors);
   } catch (e) {
-    console.log(e);
+    console.log(e, 'error');
   }
   next(err);
 };
