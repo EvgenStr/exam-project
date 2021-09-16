13055c0cd3c4f1c13642c9604f54b611e443face
EvgenStr <juve2910@gmail.com>
2021-09-16 11:32:55 +0300

    add try catch block in logErrors

--- server/src/utils/logErrors.js
+++ server/src/utils/logErrors.js
@@ -4,31 +4,35 @@ const path = require('path');
 const { LOGS_PATH } = require('../constants');
 
 module.exports = async (err, req, res, next) => {
-  const logsPath = path.resolve(LOGS_PATH, 'logs.json');
-  const { message, stack, status: code } = err;
-  const error = { message, stack, code, time: Date.now() };
+  try {
+    const logsPath = path.resolve(LOGS_PATH, 'logs.json');
+    const { message, stack, status: code } = err;
+    const error = { message, stack, code, time: Date.now() };
 
-  await access(logsPath, constants.W_OK, e => {
-    if (e) {
-      return writeFile(logsPath, JSON.stringify([error]), 'utf8', async e => {
-        if (e) throw e;
-      });
-    }
-    fs.readFile(logsPath, async function readFileCallback (err, data) {
-      if (err) {
-        // console.log(err);
-      } else {
-        const obj = await JSON.parse(data);
-        // console.log(obj, 'OBJECT+++++++');
-        obj.push(error);
-        const json = JSON.stringify(obj);
-        fs.writeFile(logsPath, json, e => {
-          if (e) throw e;
-          console.log('The file has been saved!++++++++++++++++++++++++++++++');
-        });
+    await access(logsPath, constants.W_OK, e => {
+      if (e) {
+        return writeFile(
+          logsPath,
+          JSON.stringify([error], null, 2),
+          'utf8',
+          async e => {
+            if (e) throw e;
+          },
+        );
       }
+      fs.readFile(logsPath, async (e, data) => {
+        if (!e) {
+          const oldErrors = await JSON.parse(data);
+          oldErrors.push(error);
+          const json = JSON.stringify(oldErrors, null, 2);
+          fs.writeFile(logsPath, json, e => {
+            if (e) throw e;
+          });
+        }
+      });
     });
-  });
-  console.log(err.stack, 'LOGER+++++++');
+  } catch (e) {
+    console.log(e);
+  }
   next(err);
 };
