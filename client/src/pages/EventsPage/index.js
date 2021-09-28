02683d02bf43916d68417c4258a923ffbcfd5934
EvgenStr <juve2910@gmail.com>
2021-09-28 15:45:06 +0300

    create EventsPage

---
+++ client/src/pages/EventsPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Events from '../../components/Events';
+
+function EventsPage () {
+  return (
+    <div>
+      <Events />
+    </div>
+  );
+}
+export default EventsPage;
