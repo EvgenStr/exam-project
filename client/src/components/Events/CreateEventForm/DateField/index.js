5e781f13a46d82550a50b04d3d58098552f32bb0
EvgenStr <juve2910@gmail.com>
2021-09-30 17:21:32 +0300

    create DateField component

---
+++ client/src/components/Events/CreateEventForm/DateField/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-datepicker';
+import { useField, useFormikContext } from 'formik';
+import 'react-datepicker/dist/react-datepicker.css';
+
+function DateField ({ ...props }) {
+  const [startDate, setStartDate] = useState( Date.now());
+  const { setFieldValue } = useFormikContext();
+  const [field] = useField(props);
+
+  const changeHandler = date => {
+    const timestamp = new Date(date).getTime()
+    // console.log(timestamp, 'timestamp', Date.now())
+    setStartDate(timestamp);
+    setFieldValue(field.name, timestamp);
+  };
+  return (
+    <div>
+      <DatePicker
+        {...props}
+        selected={startDate}
+        onChange={changeHandler}
+        timeInputLabel='Time:'
+        dateFormat='MM/dd/yyyy H:mm'
+        showTimeInput
+        minDate={Date.now()}
+      />
+    </div>
+  );
+}
+
+export default DateField;
