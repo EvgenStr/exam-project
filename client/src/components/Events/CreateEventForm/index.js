16b5d60aadd08eebd279b78bf1c2c6faa5aeeae6
EvgenStr <juve2910@gmail.com>
2021-10-01 18:05:55 +0300

    add reminderDate field to CreateEventForm

--- client/src/components/Events/CreateEventForm/index.js
+++ client/src/components/Events/CreateEventForm/index.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import { Formik, Field } from 'formik';
 import { isAfter, formatDistance } from 'date-fns';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { createEventAction } from '../../../actions/actionCreator';
 import DateField from './DateField';
 import Schemas from '../../../validators/validationSchemas';
 
 const initialValues = {
   name: '',
-  date: '',
+  endDate: '',
+  reminderDate: '',
 };
 
 function CreateEventForm () {
+  const {
+    data: { id },
+  } = useSelector(state => state.auth);
   const dispatch = useDispatch();
-
-  const createEvent = values => {
-    dispatch(createEventAction(values));
+  const createEvent = ({ endDate, name, reminderDate }) => {
+    dispatch(
+      createEventAction({
+        name,
+        endDate: endDate.getTime(),
+        reminderDate: reminderDate.getTime(),
+        id,
+        startDate: Date.now(),
+      }),
+    );
   };
   return (
     <div>
@@ -23,18 +34,24 @@ function CreateEventForm () {
         initialValues={initialValues}
         validationSchema={Schemas.CreateEventSchema}
         onSubmit={(values, actions) => {
-          if (isAfter(values.date, new Date())) {
+          if (isAfter(values.endDate, new Date())) {
             createEvent(values);
-            // actions.resetForm()
+            actions.resetForm();
           }
         }}
       >
         {props => (
           <form onSubmit={props.handleSubmit}>
-            <Field type='text' name='name' placeholder='Event Name' />
+            <Field type='text' name='name' placeholder='Event name' />
             {props.errors.name && <div id='feedback'>{props.errors.name}</div>}
-            <DateField name='date' />
-            {props.errors.date && <div id='feedback'>{props.errors.date}</div>}
+            <DateField name='endDate' placeholderText='Event date' />
+            {props.errors.endDate && (
+              <div id='feedback'>{props.errors.endDate}</div>
+            )}
+            <DateField name='reminderDate' placeholderText='Reminder date' />
+            {props.errors.reminderDate && (
+              <div id='feedback'>{props.errors.reminderDate}</div>
+            )}
             <button type='submit'>Create Event</button>
           </form>
         )}
