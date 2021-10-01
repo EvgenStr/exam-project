928600bc91052a16fbbe6d126aeb82b57854454a
EvgenStr <juve2910@gmail.com>
2021-10-01 12:43:48 +0300

    add getEventsAction to EventList

--- client/src/components/Events/EventsList/index.js
+++ client/src/components/Events/EventsList/index.js
@@ -1,7 +1,15 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getEventsAction } from '../../../actions/actionCreator';
 
 function EventList () {
-  return <div></div>;
+  const { events } = useSelector(state => state.events);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getEventsAction());
+  }, []);
+
+  return <div>{JSON.stringify(events)}</div>;
 }
 export default EventList;
