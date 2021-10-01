87d02ea80a87e5b05ecbb2b6cb148aaeef2d464d
EvgenStr <juve2910@gmail.com>
2021-10-01 18:14:27 +0300

    add progress bar to EventsListItem component

--- client/src/components/Events/EventsList/EventsListItem/index.js
+++ client/src/components/Events/EventsList/EventsListItem/index.js
@@ -1,6 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { formatDistanceToNowStrict, isAfter } from 'date-fns';
+import styles from './EventsListItem.module.sass';
 
-function EventsListItem () {
-  return <div></div>;
+function EventsListItem ({ event }) {
+  const duration = event.endDate - event.startDate;
+  const [progress, setProgress] = useState(
+    Math.ceil(100 - ((event.endDate - Date.now()) / duration) * 100),
+  );
+  const getTimeLeft = () => {
+    if (isAfter(event.endDate, Date.now())) {
+      return formatDistanceToNowStrict(event.endDate);
+    }
+    return 'time is over';
+  };
+  const timeLeft = getTimeLeft();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log('test', progress);
+      setProgress(
+        Math.ceil(100 - ((event.endDate - Date.now()) / duration) * 100),
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [progress]);
+
+  return (
+    <div className={styles.container}>
+      <span> {timeLeft}</span>
+      <div className={styles.progress} style={{ width: `${progress}%` }}>
+        <span> {event.name}</span>
+      </div>
+    </div>
+  );
 }
 export default EventsListItem;
