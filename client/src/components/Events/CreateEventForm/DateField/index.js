e05d6ab52c69c2d2aa732ffccb3248403464f723
EvgenStr <juve2910@gmail.com>
2021-10-01 18:07:24 +0300

    limit the maximum date of the event

--- client/src/components/Events/CreateEventForm/DateField/index.js
+++ client/src/components/Events/CreateEventForm/DateField/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import { useField, useFormikContext } from 'formik';
+import { addYears } from 'date-fns';
 import 'react-datepicker/dist/react-datepicker.css';
 
 function DateField ({ ...props }) {
-  const [startDate, setStartDate] = useState( Date.now());
+  const [startDate, setStartDate] = useState(Date.now());
   const { setFieldValue } = useFormikContext();
   const [field] = useField(props);
 
@@ -24,6 +25,7 @@ function DateField ({ ...props }) {
         dateFormat='MM/dd/yyyy H:mm'
         showTimeInput
         minDate={Date.now()}
+        maxDate={addYears(Date.now(), 1)}
       />
     </div>
   );
