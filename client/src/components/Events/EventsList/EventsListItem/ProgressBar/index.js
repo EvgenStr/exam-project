3701c5c7ab2655722304303566677a8112b1393d
EvgenStr <juve2910@gmail.com>
2021-10-05 18:06:57 +0300

    fix ProgressBar styles

--- client/src/components/Events/EventsList/EventsListItem/ProgressBar/index.js
+++ client/src/components/Events/EventsList/EventsListItem/ProgressBar/index.js
@@ -4,10 +4,12 @@ import styles from './ProgressBar.module.sass';
 function ProgressBar ({ timeLeft, progress, name }) {
   return (
     <div className={styles.container}>
+      <span className={styles.name}> {name}</span>
       <span className={styles.timeLeft}> {timeLeft}</span>
-      <div className={styles.progress} style={{ width: `${progress}%` }}>
-        <span> {name}</span>
-      </div>
+      <div
+        className={styles.progress}
+        style={{ width: `${progress < 100 ? progress : 100}%` }}
+      ></div>
     </div>
   );
 }
