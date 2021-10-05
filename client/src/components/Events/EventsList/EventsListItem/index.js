cad46936bef4e8771f57fe4865d7968a55411717
EvgenStr <juve2910@gmail.com>
2021-10-05 13:00:55 +0300

    add delete event button

--- client/src/components/Events/EventsList/EventsListItem/index.js
+++ client/src/components/Events/EventsList/EventsListItem/index.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { formatDistanceToNowStrict, isAfter } from 'date-fns';
+import ProgressBar from './ProgressBar';
+import CONSTANTS from '../../../../constants';
+import { deleteEventAction } from '../../../../actions/actionCreator';
 import styles from './EventsListItem.module.sass';
 
 function EventsListItem ({ event }) {
+  const dispatch = useDispatch();
   const duration = event.endDate - event.startDate;
   const calcProgress = () =>
     Math.ceil(100 - ((event.endDate - Date.now()) / duration) * 100);
@@ -26,10 +31,14 @@ function EventsListItem ({ event }) {
 
   return (
     <div className={styles.container}>
-      <span> {timeLeft}</span>
-      <div className={styles.progress} style={{ width: `${progress}%` }}>
-        <span> {event.name}</span>
-      </div>
+      <ProgressBar timeLeft={timeLeft} progress={progress} name={event.name} />
+      <button
+        onClick={() => {
+          dispatch(deleteEventAction(event.startDate));
+        }}
+      >
+        <img src={`${CONSTANTS.STATIC_IMAGES_PATH}icons/trash-can.svg`} alt='delete' />
+      </button>
     </div>
   );
 }
