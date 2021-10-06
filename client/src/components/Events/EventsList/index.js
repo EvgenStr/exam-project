a410432ff7c290244dd48c81362560dfe20547fe
EvgenStr <juve2910@gmail.com>
2021-10-06 14:53:25 +0300

    move sort function to reducer

--- client/src/components/Events/EventsList/index.js
+++ client/src/components/Events/EventsList/index.js
@@ -1,24 +1,20 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getEventsAction } from '../../../actions/actionCreator';
+import { clearEventsBadgesAction } from '../../../actions/actionCreator';
 import EventsListItem from './EventsListItem';
-import styles from './EventsList.module.sass'
+import styles from './EventsList.module.sass';
 
 function EventsList () {
-  const {
-    events: { events },
-    auth: {
-      data: { id },
-    },
-  } = useSelector(state => state);
+  const { events } = useSelector(state => state.events);
   const dispatch = useDispatch();
-  events.sort((a, b) =>a.endDate - b.endDate);
+
   const eventsList = events.map(event => (
     <EventsListItem key={event.startDate} event={event} />
   ));
+  
   useEffect(() => {
-    dispatch(getEventsAction(id));
-  }, [dispatch, id]);
+    dispatch(clearEventsBadgesAction());
+  }, []);
 
   return <div className={styles.container}>{eventsList}</div>;
 }
