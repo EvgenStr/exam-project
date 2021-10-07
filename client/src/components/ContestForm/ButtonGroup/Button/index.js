52c0f52d42530d3a20dfb65c459c7b989f8bb299
EvgenStr <juve2910@gmail.com>
2021-10-07 15:56:16 +0300

    add layout and styles for  Button component

--- client/src/components/ContestForm/ButtonGroup/Button/index.js
+++ client/src/components/ContestForm/ButtonGroup/Button/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import cx from 'classnames';
+import styles from './Button.module.sass';
 
-function Button () {
-  return <div></div>;
+function Button ({ data: { badge, description, value }, active, handler }) {
+  console.log(active, 'button');
+  return (
+    <div
+      className={cx(styles.container, value === active ? styles.active : '')}
+      onClick={() => {
+        handler(value);
+      }}
+    >
+      <div className={styles.card}>
+        <div className={styles.badgeContainer}>
+          <span
+            className={cx(
+              styles.badge,
+              value === active ? styles.activeBadge : '',
+            )}
+          >
+            {badge}
+          </span>
+        </div>
+        <h5 className={styles.description}>{description}</h5>
+      </div>
+    </div>
+  );
 }
 
 export default Button;
