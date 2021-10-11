a55a4e945f0ad3ae5d4f4df72ea855476b1da533
EvgenStr <juve2910@gmail.com>
2021-10-11 12:37:28 +0300

    create Banner component and styles

--- client/src/pages/HowItWorksPage/index.js
+++ client/src/pages/HowItWorksPage/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import styles from './HowItWorksPage.module.sass'
 
 function HowItWorksPage () {
-  return <>
-    <Header/>
-  </>;
+  return (
+    <>
+      <Header />
+      <main>
+        <section className={styles.banner}></section>
+      </main>
+    </>
+  );
 }
 
 export default HowItWorksPage;
