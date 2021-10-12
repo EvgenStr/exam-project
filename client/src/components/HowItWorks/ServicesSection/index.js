1a7a56337f59730d2661e4f54023c85c4d5fdc17
EvgenStr <juve2910@gmail.com>
2021-10-12 12:35:18 +0300

    create cardsData.json

--- client/src/components/HowItWorks/ServicesSection/index.js
+++ client/src/components/HowItWorks/ServicesSection/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import styles from './ServicesSection.module.sass';
 
 function ServicesSection () {
-  return <div></div>;
+  return (
+    <div className={'container'}>
+      <div className={'w-md-80 w-lg-80 text-center mx-md-auto mb-5'}>
+        <small class='btn btn-xs btn-soft-primary btn-pill mb-2'>
+          Our Services
+        </small>
+        <h2 class='font-weight-normal'>3 Ways To Use Squadhelp</h2>
+        <p class='mb-0'>
+          Squadhelp offers 3 ways to get you a perfect name for your business.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default ServicesSection;
