b3a4b38873ddbec8ad0d173e779d246d59aedb26
EvgenStr <juve2910@gmail.com>
2021-10-13 11:49:35 +0300

    create FAQTopics components

--- client/src/pages/HowItWorksPage/index.js
+++ client/src/pages/HowItWorksPage/index.js
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header';
 import Banner from '../../components/HowItWorks/Banner';
 import ServicesSection from '../../components/HowItWorks/ServicesSection';
 import FeaturesSection from '../../components/HowItWorks/FeaturesSection';
+import FAQTopics from '../../components/HowItWorks/FAQTopics';
 import styles from './HowItWorksPage.module.sass';
 
 function HowItWorksPage () {
@@ -13,6 +14,8 @@ function HowItWorksPage () {
         <Banner />
         <ServicesSection />
         <FeaturesSection />
+        <hr className={'my-0'} />
+        <FAQTopics />
       </main>
     </>
   );
