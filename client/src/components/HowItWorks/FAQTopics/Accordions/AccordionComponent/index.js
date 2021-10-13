3beb8105991fc89bfaa13f465ba51cf357217fb9
EvgenStr <juve2910@gmail.com>
2021-10-13 14:45:37 +0300

    create Accordion Component

---
+++ client/src/components/HowItWorks/FAQTopics/Accordions/AccordionComponent/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Accordion } from 'react-bootstrap';
+
+function AccordionComponent ({ id, title, data }) {
+  const items = data.map((item, i) => {
+    return (
+      <Accordion.Item eventKey={i} className={'mb-3 border-top'}>
+        <Accordion.Header>{item.head}</Accordion.Header>
+        <Accordion.Body>{item.content}</Accordion.Body>
+      </Accordion.Item>
+    );
+  });
+  return (
+    <div id={id}>
+      <h3 className='text-primary fw-bold'>{title}</h3>
+      <Accordion defaultActiveKey={0}>{items}</Accordion>
+    </div>
+  );
+}
+
+export default AccordionComponent;
