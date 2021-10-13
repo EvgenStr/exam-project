b7cf4873837a9ea7c2257065be2ac6715fc9ac24
EvgenStr <juve2910@gmail.com>
2021-10-13 12:08:30 +0300

    create Nav component

--- client/src/components/HowItWorks/FAQTopics/index.js
+++ client/src/components/HowItWorks/FAQTopics/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Nav from './Nav'
 
 function FAQTopics () {
-  return <div></div>;
+  return (
+    <section className={'container-lg'}>
+      <div className={'row'}>
+        <div className={'col-lg-3 mb-5 mb-lg-0'}>
+
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default FAQTopics;
