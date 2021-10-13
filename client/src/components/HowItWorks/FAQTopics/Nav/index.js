423efe6533ccae40dbbdc7431b994e3b71eeea41
EvgenStr <juve2910@gmail.com>
2021-10-13 13:05:47 +0300

    add Nav in FAQTopics component

--- client/src/components/HowItWorks/FAQTopics/Nav/index.js
+++ client/src/components/HowItWorks/FAQTopics/Nav/index.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from 'react';
+import NavItem from './NavItem';
+import navData from './navData.json';
 
-function Nav() {
+function Nav () {
+  const navItems = navData.map((item, i) => <NavItem key={i} {...item} />);
   return (
-    <div>
-      
-    </div>
-  )
+    <nav className='card border-0 bg-primary'>
+      <ul className='list-group list-group-transparent list-group-white list-group-flush list-group-borderless py-3 px-4'>
+        {navItems}
+      </ul>
+    </nav>
+  );
 }
 
-export default Nav
+export default Nav;
--- client/src/components/HowItWorks/FAQTopics/index.js
+++ client/src/components/HowItWorks/FAQTopics/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Nav from './Nav'
+import Nav from './Nav';
 
 function FAQTopics () {
   return (
-    <section className={'container-lg'}>
+    <section className={'container-lg mt-5'}>
       <div className={'row'}>
         <div className={'col-lg-3 mb-5 mb-lg-0'}>
-
+          <Nav />
         </div>
       </div>
     </section>
