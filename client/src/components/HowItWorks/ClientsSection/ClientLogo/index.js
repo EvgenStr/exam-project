c8d4db5651e28510c60c93be05d2df204d49a154
EvgenStr <juve2910@gmail.com>
2021-10-15 15:51:26 +0300

    add styles and layout for ClientLogo component

--- client/src/components/HowItWorks/ClientsSection/ClientLogo/index.js
+++ client/src/components/HowItWorks/ClientsSection/ClientLogo/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import cx from 'classnames';
+import styles from './ClientLogo.module.sass'
+import CONSTANTS from '../../../../constants';
 
-function ClientLogo () {
+function ClientLogo ({ name, imgSrc }) {
   return (
     <div className='col-4 col-lg-3 mb-4 mb-lg-0'>
       <div className='mx-2'>
-        <a href="https://google.com">
+        <a href='https://google.com'>
           <img
-            className='u-clients lazy loaded'
-            alt='forbes'
-            src='/resources/assets/imgs/front/forbes.svg'
-
+            className={cx(styles.client)}
+            alt={name}
+            src={`${CONSTANTS.STATIC_IMAGES_PATH}${imgSrc}`}
           />
         </a>
       </div>
